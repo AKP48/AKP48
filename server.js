ee7bcb01ada8894ccab63056d8ffd80d1ed3fc28
Austin Peterson <austin@akpwebdesign.com>
2014-12-21 12:48:35 -0600

    Add productionMode config option for catching all exceptions.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var ClientManager = require('./clientmanager');
 
 require('./polyfill.js')();
+var config = require('./config.json');
 
-var climgr = new ClientManager(require('./config.json'));
+var climgr = new ClientManager(config);
 
 //todo: better exception handling plz
+if(config.productionMode) {
+    process.on('uncaughtException', function(err) {
+        console.log('Caught exception: ' + err);
+    });
+}
