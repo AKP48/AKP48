503e591448ef38f37730bdde57dcf35cbb81098f
Austin Peterson <austin@akpwebdesign.com>
2015-01-20 10:22:11 -0600

    Support downloading code from a URL for JS command.

--- Commands/js.js
+++ Commands/js.js
@@ -17,6 +17,11 @@
 
 var Sandbox = require("sandbox");
 var s = new Sandbox();
+var urlRegEx = require("../Regex/regex-weburl");
+var request = require('request');
+var Chance = require('chance');
+var path = require('path');
+var fs = require('fs');
 
 function Js() {
     //the name of the command.
@@ -39,11 +44,67 @@ function Js() {
 
     //whether or not to only allow this command if it's in a private message.
     this.isPmOnly = false;
+
+    //the maximum size of a downloaded file.
+    this.maxSize = 10485760;
+
+    //randomizer
+    this.chance = new Chance();
 }
 
 Js.prototype.execute = function(context) {
+    //if no args, return false
     if(!context.arguments.length){return false};
-    s.run( context.arguments.join(" "), function(output) {
+
+    //if first argument is a url, attempt to download file.
+    if(context.arguments[0].match(urlRegEx)) {
+        var self = this;
+        //HEAD request to get size.
+        request({url: context.arguments[0], method: "HEAD"}, function(err, headRes) {
+            var size = headRes.headers['content-length'];
+            //if size is too big, return.
+            if (size > this.maxSize) {
+                return true;
+            } else {
+                //if size is okay, make actual request now.
+                var filename = path.join(path.resolve('tmp/js'), self.chance.word({syllables: 4})+".js");
+                var file = fs.createWriteStream(filename),
+                    size = 0;
+
+                var res = request({ url: context.arguments[0] });
+
+                res.on('data', function(data) {
+                    size += data.length;
+
+                    //if size is too big...
+                    if (size > this.maxSize) {
+                        res.abort(); // Abort the response (close and cleanup the stream)
+                        fs.unlink(filename); // Delete the file we were downloading the data to
+                        return true;
+                    }
+                }).pipe(file);
+
+                res.on('end', function(){
+                    self.runCode(fs.readFileSync(filename, {encoding: "utf8"}), context); //run code.
+                    fs.unlink(filename); //delete file after running code.
+                });
+            }
+        });
+    } else {
+        //run code in sandbox, output results.
+        this.runCode(context.arguments.join(" "), context);
+    }
+
+    return true;
+};
+
+/**
+ * Runs JS code in a sandbox.
+ * @param  {String}  code    The code to run.
+ * @param  {Context} context The context to send response to.
+ */
+Js.prototype.runCode = function(code, context) {
+    s.run(code, function(output) {
         var outputString = "";
         if(output.result) {
             outputString += output.result + "; ";
@@ -57,7 +118,6 @@ Js.prototype.execute = function(context) {
         }
         context.getClient().say(context, outputString);
     });
-    return true;
 };
 
 module.exports = Js;
