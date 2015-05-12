c36c26938d972ce9c0b3d26ed53f4b5580f8b790
Austin Peterson <austin@akpwebdesign.com>
2015-05-12 01:11:01 -0500

    Properly create channel upon invite, so that commands can be run in the new channels.

--- Client/Client.js
+++ Client/Client.js
@@ -321,7 +321,7 @@ Client.prototype.initialize = function(clientManager, holdIRCClient) {
     });
 
     this.ircClient.on('invite', function(channel, from, message) {
-        self.addChannel({name: channel, commandDelimiter: "."});
+        self.addChannel(channel);
         self.getIRCClient().join(channel, function(){
             self.getIRCClient().say(channel, "Thanks for inviting me, "+from+"! I'm glad to be here. For more information about me, say `.help`.");
         });
